903fc77a26a62bf302e97265fdb91a090de58412
Darshan A <darshanveerapur986@gmail.com>
2026-08-11 18:01:35 +0530

    Implented getChequeByNumber

--- src/com/iispl/dao/ChequeDAOImpl.java
+++ src/com/iispl/dao/ChequeDAOImpl.java
@@ -81,12 +81,53 @@ public class ChequeDAOImpl implements ChequeDAO {
 	@Override
 	public Cheque getChequeByNumber(String chequeNumber) {
 		// TODO Auto-generated method stub
-		return null;
+		
+
+		String sql = " SELECT * FROM CTS_CHEQUE WHERE cheque_number = ?";
+	    Cheque cheque = null;
+
+		try {
+			Connection connection = DBUtils.getDataSource().getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+	        statement.setString(1, chequeNumber);
+			ResultSet rs = statement.executeQuery();
+			while(rs.next()) {
+				 cheque = new Cheque();
+				
+				cheque.setChequeId(rs.getInt("cheque_id"));
+				cheque.setChequeNumber(rs.getString("cheque_number"));
+				cheque.setAccountNumber(rs.getString("account_number"));
+				cheque.setCustomerName(rs.getString("customer_name"));
+				cheque.setBranchCode(rs.getString("branch_code"));
+				cheque.setMicrCode(rs.getString("micr_code"));
+				cheque.setAmount(rs.getBigDecimal("amount"));
+				cheque.setAvailableBalance(rs.getBigDecimal("available_balance"));
+				cheque.setChequeDate(rs.getDate("cheque_date").toLocalDate());
+				cheque.setAccountStatus(
+					    AccountStatus.valueOf(rs.getString("account_status")));
+				cheque.setChequeType(
+						ChequeType.valueOf(rs.getString("cheque_type")));
+				cheque.setMicrStatus(
+					    MicrStatus.valueOf(rs.getString("micr_status")));
+				cheque.setValidationStatus(
+					    ValidationStatus.valueOf(rs.getString("validation_status")));
+				cheque.setBatchId(rs.getInt("batch_id"));
+					
+			    
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
+		return cheque;
 	}
+	
 
 	@Override
 	public void updateMicrStatus(String chequeNumber, MicrStatus status) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubgit
 		
 	}
 
